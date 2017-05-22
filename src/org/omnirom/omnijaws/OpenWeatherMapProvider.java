e4ab3baac90d80f9439109586e4fbc91bc6d689d
Ali B <abittin@gmail.com>
2017-05-22 10:11:47 +0300

    Switch to AICP OWM key -- READ COMMIT MESSAGE
    Openweathermap provides access to weather only and only if the
    requester uses a registered API key and obeys the following rules:
    (https://openweathermap.org/appid#get)
    - Do not send requests more than 1 time per 10 minutes from one
      device/one API key. Normally the weather is not changing
      so frequently.
    - Free and Startup accounts have limitation of capacity and data
      availability. If you do not get respond from server do not try
      to repeat your request immediately, but only after 10 min.
    
    So, dont pick this commit PLEASE. Instead, visit the OWM site using
    above link to get yourself a personal API key.
    
    Using ours will surely block OWM service for the rest of the users.
    So, BE CONSIDERATE!
    
    Thank you
    AICP TEAM
    
    Change-Id: I4adb74d0828a7c49aa8c85768e09acf3fcd3080d

--- src/org/omnirom/omnijaws/OpenWeatherMapProvider.java
+++ src/org/omnirom/omnijaws/OpenWeatherMapProvider.java
@@ -38,7 +38,7 @@ public class OpenWeatherMapProvider extends AbstractWeatherProvider {
     private static final int FORECAST_DAYS = 5;
     private static final String SELECTION_LOCATION = "lat=%f&lon=%f";
     private static final String SELECTION_ID = "id=%s";
-    private static final String API_KEY = "9f65b19b7a6648346dda93c6973a682c";
+    private static final String API_KEY = "bf2e711833265643fa9749dfbc9c2d54";
 
     private static final String URL_LOCATION =
             "http://api.openweathermap.org/data/2.5/find?q=%s&mode=json&lang=%s&appid=%s";
